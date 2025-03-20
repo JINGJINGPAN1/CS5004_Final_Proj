358bccdf164b8e50a22d2f85649332babe5b974a
Jingjing Pan <pan_jingjing@foxmail.com>
2025-03-20 13:16:46 -0700

    Teaming and Elevator Pitch completed

--- GoldMiner/src/view/GameWin.java
+++ GoldMiner/src/view/GameWin.java
@@ -2,8 +2,6 @@ package view;
 
 import controller.GameController;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
